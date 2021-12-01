002eabfd9ab7476a668cde0492a21826c17fff00
DaemoniumRex <91150594+DaemoniumRex@users.noreply.github.com>
2021-11-30 20:39:23 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:getglam/ui/routes/app_routes.dart';
+import 'package:getglam/ui/routes/app_pages.dart';
+import 'package:getglam/themes/app_themes.dart';
 
-import'home.dart';
-//import 'login.dart'
 
-void main () => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //theme: ThemeData.dark(),
-      home: HomeScreen(),
-    );
-  }
+void main() {
+  runApp( MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return GetMaterialApp(
+      title: "GetGlam",
+      initialRoute: AppRoutes.DASHBOARD,
+      getPages: AppPages.list,
+      debugShowCheckedModeBanner: false,
+      darkTheme: AppThemes.dark,
+      theme: AppThemes.light,
+      themeMode: ThemeMode.system,
+    ) ;
+  }
+
+
+}
